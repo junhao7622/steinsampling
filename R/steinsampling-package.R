f8452ab8b8d59abd354e5b539e7f8c13a25f9759
junhao7622 <jug049@ucsd.edu>
2026-07-09 22:45:08 -0700

    Delete R directory

--- R/steinsampling-package.R
+++
@@ -1,8 +0,0 @@
-#' steinsampling package
-#'
-#' Kernel Stein discrepancy goodness-of-fit tests and Stein-method-based
-#' sampling methods.
-#'
-#' @keywords internal
-#' @import stats graphics
-"_PACKAGE"
